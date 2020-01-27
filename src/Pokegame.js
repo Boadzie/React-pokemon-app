cdaf74a668e3db62325db833dddf401dc70cb3b0
Boadzie Daniel <boadziedaniel43@gmail.com>
2020-01-27 09:19:56 +0000

    the full app

---
+++ src/Pokegame.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Pokegame extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pokegame</h1>
+            </div>
+        )
+    }
+}
